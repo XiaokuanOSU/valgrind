3acfa6b11f412df1a6a8673b1d4c2f3b9dac1ff1
XiaokuanOSU <zhang.5840@osu.edu>
2016-02-24 15:52:31 -0500

    Inst for different platforms, plus some scripts

---
+++ valgrind_start/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+PACKAGE="zhangx.kb3"
+
+adb push start_valgrind.sh /data/local/
+#adb shell chmod 777 /data/local/start_valgrind.sh 
+
+adb root
+adb shell setprop wrap.$PACKAGE "logwrapper\ /data/local/start_valgrind.sh"
+
+echo "wrap.$PACKAGE: $(adb shell getprop wrap.$PACKAGE)"
+
+adb shell am force-stop $PACKAGE
+adb shell am start -a android.intent.action.MAIN -n "zhangx.kb3/.MainActivity"
+
+adb logcat -c
+adb logcat
+
+exit 0 
---
+++ valgrind_start/start_valgrind.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+
+PACKAGE="zhangx.kb3"
+
+# Callgrind tool
+VGPARAMS=' --tool=callgrind --trace-children=no --error-limit=no --compress-strings=no --log-file=/sdcard/valgrind_log/valgrind.log.%p  --callgrind-out-file=/sdcard/valgrind_log/callgrind.out.%p'
+#--kernel-variant=android-no-hw-tls
+# Memcheck tool
+#VGPARAMS='--tool=memcheck --error-limit=no --log-file=/sdcard/valgrind_log/valgrind.log.%p'
+#--kernel-variant=android-no-hw-tls
+export TMPDIR=/data/data/$PACKAGE
+
+adb shell -c /data/local/Inst/bin/valgrind $VGPARAMS $* 
